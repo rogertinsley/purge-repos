68af2d27b0541826a4da5b6ca72549a74b15e24a
Roger Tinsley <mail@rogertinsley.co.uk>
2016-03-17 20:15:42 +0000

    :recycle:

---
+++ purge-repos/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using Octokit;
+
+namespace purge_repos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new GitHubClient(new ProductHeaderValue("purge-repos"))
+            {
+                Credentials = new Credentials("username", "password")
+            };
+
+            var allForCurrent = client.Repository.GetAllForCurrent().Result;
+
+            var repos = allForCurrent
+                .Where(current => current.Name.Equals("repository-name"))
+                .ToList();
+
+            foreach (var repo in repos)
+            {
+                Console.WriteLine("Attempting to delete: {0}", repo.FullName);
+
+                client.Repository.Delete("owner", repo.Name).Wait();
+                Console.WriteLine("Deleted: {0}", repo.Name);
+            }
+
+            Console.WriteLine("Done!");
+        }
+    }
+}
